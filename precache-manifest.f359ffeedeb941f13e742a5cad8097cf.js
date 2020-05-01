9fdc1206f2855a6e81beecce95d34f9192ed29dd
parechayui <parechay.ui@gmail.com>
2020-05-01 12:25:08 -0400

    Updates

---
+++ precache-manifest.f359ffeedeb941f13e742a5cad8097cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "591612c73c7db81c90869ced0ec5fdb1",
+    "url": "/parechayui/asgard.git/index.html"
+  },
+  {
+    "revision": "6e868e162aa145cb0d07",
+    "url": "/parechayui/asgard.git/static/css/main.92db3648.chunk.css"
+  },
+  {
+    "revision": "418ba3390ac35565806b",
+    "url": "/parechayui/asgard.git/static/js/2.860113bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/parechayui/asgard.git/static/js/2.860113bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e868e162aa145cb0d07",
+    "url": "/parechayui/asgard.git/static/js/main.e1832675.chunk.js"
+  },
+  {
+    "revision": "c0421ffbe231747a3b10",
+    "url": "/parechayui/asgard.git/static/js/runtime-main.918b3560.js"
+  }
+]);
